255ce482ce427ece403effbf4e6c71e400be226c
Rishit123344 <bramarasunku@gmail.com>
2020-01-26 12:51:30 +0530

    CarRacingGame

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var ball;
-
+var database ;
+var position ;
 function setup(){
+    database = firebase.database ();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballposition = database.ref ('ball/position');
+    ballposition.on ("value",readPosition,showError);
 }
 
 function draw(){
@@ -23,7 +27,17 @@ function draw(){
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function changePosition(x,y){database.ref('ball/position').set({
+    'x':position.x+x,
+    'y' :position.y+y,
+})
+    
 }
+
+function readPosition (data){
+    position = data.val ();
+    ball.x = position.x ;
+    ball.y = position.y;
+}
+function showError()
+{console.log("error in writingto the database")}
